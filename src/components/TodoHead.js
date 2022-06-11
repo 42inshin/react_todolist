26217cff2d8d239f91372d68ae7a5a92a0372bf4
inshin <inshin@student.42seoul.kr>
2022-06-11 14:54:37 +0900

    prettier set

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -3,35 +3,42 @@ import styled from 'styled-components';
 import { useTodoState } from '../TodoContext';
 
 const TodoHeadBlock = styled.div`
-	padding: 48px 32px 24px 32px;
-	border-bottom: 1px solid #e9ecef;
-	h1 {
-		margin: 0;
-		font-size: 36px;
-		color: #343a40;
-	}
-	.day {
-		margin-top: 4px;
-		color: #868e96;
-	}
-	.tasks-left {
-		color: #20c997;
-		font-size: 18px;
-		margin-top: 40px;
-		font-weight:bold;
-	}
+  padding: 48px 32px 24px 32px;
+  border-bottom: 1px solid #e9ecef;
+  h1 {
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+  .day {
+    margin-top: 4px;
+    color: #868e96;
+  }
+  .tasks-left {
+    color: #20c997;
+    font-size: 18px;
+    margin-top: 40px;
+    font-weight: bold;
+  }
 `;
 
 function TodoHead() {
-	const todos = useTodoState();
-	console.log(todos);
-	return (
-		<TodoHeadBlock>
-			<h1>2019년 7월 10일</h1>
-			<div className="day">수요일</div>
-			<div className="tasks-left">할 일 2개 남음</div>
-		</TodoHeadBlock>
-	)
+  const todos = useTodoState();
+  const undoneTasks = todos.filter((todo) => !todo.done);
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+  return (
+    <TodoHeadBlock>
+      <h1>2019년 7월 10일</h1>
+      <div className="day">수요일</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
+    </TodoHeadBlock>
+  );
 }
 
 export default TodoHead;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
-	flex: 1;
-	padding: 20px 32px 48px;
-	overflow-y: auto;
-	/* background: gray; */
+  flex: 1;
+  padding: 20px 32px 48px;
+  overflow-y: auto;
+  /* background: gray; */
 `;
 
 function TodoList() {
